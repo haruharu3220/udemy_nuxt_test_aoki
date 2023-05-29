2a5c2921ef000c8c07c9555e48a4649ba9e307b1
Hayato <hayato.314.nico@gmail.com>
2023-05-30 06:49:37 +0900

    $routerと$routeの違い

--- Vue2_Vue3_practice/section7/vuerouter/src/views/AboutView.vue
+++ Vue2_Vue3_practice/section7/vuerouter/src/views/AboutView.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="about">
+  <div class="aboutVue">
     <h1>This is an about page</h1>
+    <button @click="checkRouteInfo">ルート情報</button>
+    <button @click="gotoHome">homeに戻る</button>
   </div>
 </template>
+<script>
+export default {
+  name: "AboutVue",
+  methods: {
+    checkRouteInfo() {
+      console.log(this.$route);
+      console.log(this.$route.path);
+      // console.log(this.)
+    },
+    gotoHome() {
+      this.$router.push("/");
+    },
+  },
+};
+</script>
